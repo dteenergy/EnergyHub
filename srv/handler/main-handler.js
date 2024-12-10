3c1cb3a4e62589bf1eb1d08b177c52fdd82d5eec
Divya-Kaavian <divyadharshini.murugan@kaaviansys.com>
2024-12-10 09:09:48 +0530

    DTE Energy Hub- Enrollment form handler message updated

--- srv/handler/main-handler.js
+++ srv/handler/main-handler.js
@@ -6,15 +6,17 @@ module.exports = cds.service.impl(async function(srv){
     
     srv.on('CreateEnrollmentFormDetail',  async(req)=>
         {
+            // Initialize the transaction
             const tx = cds.tx(req);
             
             try{
+                // Method to create the Enrollment Form details
                 const res = createEnrollmentFormDetail(req, this.entities, tx)
 
                 return res;
             } catch(e){
-
-                return {'error':'Failed to create'}
+                
+                return {'error':'Failed to create Enrollment Form'}
             }
             
         })   
