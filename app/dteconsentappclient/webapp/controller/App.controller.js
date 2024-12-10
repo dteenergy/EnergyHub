2a25101afa7f1fff8e509e8f174d1a412881b288
Kokila ss <kokila.ss@smartsoftus.com>
2024-12-10 01:46:03 +0000

    added enrollment form segments

--- app/dteconsentappclient/webapp/controller/App.controller.js
+++ app/dteconsentappclient/webapp/controller/App.controller.js
@@ -8,16 +8,25 @@ sap.ui.define([
       onInit() {
         const oSocialMediaModel = new JSONModel({
           sMediaDetails: [
-            {'path': 'images/Facebook.png', alt:'Facebook'},
-            {'path': 'images/X-Twitter.png', alt:'X-Twitter'},
-            {'path': 'images/Instagram.png', alt:'Instagram'},
-            {'path': 'images/YouTube.png', alt:'Youtube'},
-            {'path': 'images/LinkedIn.png', alt:'LinkedIn'}
+            {'path': 'images/Facebook.png', 'alt':'Facebook', 'src':''},
+            {'path': 'images/X-Twitter.png', 'alt':'X-Twitter', 'src':''},
+            {'path': 'images/Instagram.png', 'alt':'Instagram', 'src':''},
+            {'path': 'images/YouTube.png', 'alt':'Youtube', 'src':''},
+            {'path': 'images/LinkedIn.png', 'alt':'LinkedIn', 'src':'https://en.wikipedia.org/wiki/Heello'}
           ]
         });
 
         this.getView().setModel(oSocialMediaModel, 'socialMediaModel')
         
+      },
+
+      onSocialMedia: function(oEvent){
+        const mediaDetails = this.getView().getModel('socialMediaModel').getData();
+        
+        const salt = oEvent.getSource().getAlt();
+        const filteredData = mediaDetails?.sMediaDetails.filter((mediaDetail)=> mediaDetail.alt === salt);
+        
+        window.open(filteredData[0]['src'], '_blank');
       }
   });
 });
--- app/dteconsentappclient/webapp/controller/Landlord.controller.js
+++ app/dteconsentappclient/webapp/controller/Landlord.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "dteconsentappclient/controller/BaseController",
-    "sap/ui/core/Fragment"
-], (BaseController, Fragment) => {
+    "sap/ui/core/Fragment",
+    "sap/ui/model/json/JSONModel"
+], (BaseController, Fragment, JSONModel) => {
     "use strict";
 
     return BaseController.extend("dteconsentappclient.controller.Landlord", {
@@ -10,6 +11,52 @@ sap.ui.define([
           const { url, headers } = this.getApiConfig();
           this.HEADERS = headers;
           this.SERVERHOST = url;
+
+          let oEnrollFormData = {
+            SignatureSignedBy: "",
+            SignatureSignedDate: "",
+            AccountDetail: {}
+        };
+
+        // Set the JSONModel with the correct name
+        const oEnrollModel = new JSONModel(oEnrollFormData);
+        this.getView().setModel(oEnrollModel, "enrollModel");
+
+        const oModel = new JSONModel({
+            locations: [], // Array to hold all building location data
+        });
+        this.getView().setModel(oModel, "locationModel");
+        this.onAddAnotherLocation();
+        },
+
+        onAddAnotherLocation: function(){
+            const oView = this.getView();
+            const oModel = oView.getModel("locationModel");
+            const buildingmainContainer = this.byId("building_detail_main_container");
+            const locations = oModel.getProperty("/locations");
+
+            locations.push({
+                buildingName: "",
+                accountNumber: "",
+                locationAddress: "",
+                city: "",
+                state: "Michigan",
+                zipcode: ""
+            });
+            oModel.setProperty("/locations", locations);
+
+            const index = locations.length - 1;
+
+            Fragment.load({
+                name: "dteconsentappclient.fragment.Buildingdetail",
+                controller: this
+            }).then(function (oFragment) {
+                oFragment.setModel(oModel, "locationMoel");
+                oFragment.bindElement(`locationModel>/locations/${index}`);
+                buildingmainContainer.addItem(oFragment);
+            }).catch(function (err) {
+                console.log(`Failed to load fragment:, ${err}`)
+            });
         },
 
         handleSubmit: async function () {
