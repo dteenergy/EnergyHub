7ce4b656be0efa8a7594f3ee660ffb68aaf0cf41
Divya-Kaavian <divyadharshini.murugan@kaaviansys.com>
2024-12-10 22:41:22 +0530

    DTE-ENERGY-Enrollment form : Review corrections are done

--- srv/handler/create-enrollment-form-action.js
+++ srv/handler/create-enrollment-form-action.js
@@ -8,56 +8,47 @@ const { v4: uuidv4 } = require('uuid');
  * tx => function
  */
 const createEnrollmentFormDetail = async (req, entity, tx) => {
-  
+
   try {
     // Generate a unique AppId using uuid
     const AppId = uuidv4();
 
     // Get the payload from the req and variable declaration
-    const { ApplicationDetail, BuildingDetail, AccountDetail, ConsentDetail } = req?.data; 
-    
+    const { ApplicationDetail, BuildingDetail, AccountDetail, ConsentDetail } = req?.data;
+
     // Parse the String Payload
     const applicationParsedData = JSON.parse(ApplicationDetail);
     const buildingParsedData = JSON.parse(BuildingDetail);
     const accountParsedData = JSON.parse(AccountDetail);
     const consentParsedData = JSON.parse(ConsentDetail);
 
-    // Check the Object contains the empty check data and JSON object/arrays contain value or not
-    const buildingParsedDataCheck = buildingParsedData?.some(e => 
+    // Check the build details fields contains the empty value
+    const buildingDetailFieldCheck = buildingParsedData?.some(e =>
       Object.values(e).includes("")
     ) ? [] : 'Building Details Contains the Valid data';
-    
-    if((Object.keys(applicationParsedData)?.length === 0) || (buildingParsedData?.length === 0) || 
-    (buildingParsedDataCheck?.length === 0) || (Object.keys(accountParsedData)?.length === 0) || (consentParsedData?.length === 0))  
-    return { 'status': 400, 'message': 'The data is invalid. Please review and correct the erroneous fields' }
 
-    // Assign AppId to various parsed data objects and add AppRefId_AppId property where needed
+    if ((Object.keys(applicationParsedData)?.length === 0) || (buildingParsedData?.length === 0) ||
+      (buildingDetailFieldCheck?.length === 0) || (Object.keys(accountParsedData)?.length === 0) || (consentParsedData?.length === 0))
+      return { 'status': 400, 'message': 'The data is invalid. Please review and correct the erroneous fields' }
+
+    // Assign AppId to Application Detail, Building Detail, Account Detail and Application Consent 
     applicationParsedData.AppId = AppId
-    
     buildingParsedData?.map(detail => detail.AppRefId_AppId = AppId);
-        
-    accountParsedData.AppRefId_AppId = AppId;    
+    accountParsedData.AppRefId_AppId = AppId;
+    consentParsedData.map(consent => consent.AppRefId_AppId = AppId);
 
-    consentParsedData.map(consent => consent.AppRefId_AppId= AppId);    
-    
-    // Insert the applicationParsedData into the ApplicationDetail table using a transactional query
+    // Insert Enrollment Form details to database
     const applicationDetailResult = await tx.run(INSERT.into(entity?.ApplicationDetail).entries(applicationParsedData));
-
-    // Insert the buildingDetailPayload into the BuildingDetails table using a transactional query
     const buildingDetailResult = await tx.run(INSERT.into(entity?.BuildingDetail).entries(buildingParsedData));
-
-    // Insert accountParsedData into the AccountDetail table using a transactional query
     const accountDetailResult = await tx.run(INSERT.into(entity?.AccountDetail).entries(accountParsedData));
-
-    // Insert the consentDetailPayload into the ApplicationConsent table using a transactional query
     const consentDetailResult = await tx.run(INSERT.into(entity?.ApplicationConsent).entries(consentParsedData));
-      
-    // Check if results were returned for all queries: ApplicationDetail, BuildingDetail, AccountDetail, and ApplicationConsent
-    if((applicationDetailResult?.results?.length > 0) && (buildingDetailResult?.results?.length > 0)
-    && (accountDetailResult?.results?.length > 0) && (consentDetailResult?.results?.length > 0))
+
+    // Check Enrollment Form Details inserted successfully.
+    if ((applicationDetailResult?.results?.length > 0) && (buildingDetailResult?.results?.length > 0)
+      && (accountDetailResult?.results?.length > 0) && (consentDetailResult?.results?.length > 0))
       return { statusCode: 200, Message: "Enrollment form created successfully." };
-   
-    } catch (error) {
+
+  } catch (error) {
     console.log("Enrollment Form Creation Error :", error);
     return {
       statusCode: 500,
