3b851b7f9e47e50ff44bb83d4019beedba1adeb2
Divya-Kaavian <divyadharshini.murugan@kaaviansys.com>
2024-12-17 07:01:11 +0000

    DTE Energy Hub: Handle the cors origin

--- srv/server.js
+++ srv/server.js
@@ -1,6 +1,16 @@
 const cds = require('@sap/cds');
 const cors = require('cors');
+// Get the HTML5 Production URL and set to the Origins object
+let data = process.env.HTML5_PRODUCTION;
+const ORIGINS = {};
+ORIGINS[data] = 1;
 
-cds.on("bootstrap", async(app)=>{
-    app.use(cors({ origin: "*" }));
-})
+cds.on('bootstrap', app => app.use ((req, res, next) => {
+    if (req.headers.origin in ORIGINS) {
+    
+    res.set('access-control-allow-origin', req.headers.origin)
+    if (req.method === 'OPTIONS') // preflight request
+      return res.set('access-control-allow-methods', 'GET,HEAD,PUT,PATCH,POST,DELETE').end()
+  }
+  next()
+}))
