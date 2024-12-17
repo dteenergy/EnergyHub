186aae80068edd026038e4f4f9b9c8f6612b5364
Revathy Rajasekaran <revathy.rajasekaran@dteenergy.com>
2024-12-17 14:47:14 +0000

    Resolve conflict

--- app/dteconsentappclient/webapp/controller/App.controller.js
+++ app/dteconsentappclient/webapp/controller/App.controller.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller",
+  "dteconsentappclient/controller/BaseController",
   "sap/ui/model/json/JSONModel",
 ], (BaseController, JSONModel) => {
   "use strict";
