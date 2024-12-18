bbbf5595720471975c0d897eab40bcbc8aae85a2
Divya-Kaavian <divyadharshini.murugan@kaaviansys.com>
2024-12-18 05:59:08 +0000

    Updated the Env and Server.js file with the URL

--- srv/server.js
+++ srv/server.js
@@ -1,9 +1,14 @@
 const cds = require('@sap/cds');
 const cors = require('cors');
+
 // Get the HTML5 Production URL and set to the Origins object
-let data = process.env.HTML5_PRODUCTION;
+const adminUrl = process.env.ADMIN_PRODUCTION;
+const data = process.env.HTML5_PRODUCTION;
+
+// Variable to store the list of Origin
 const ORIGINS = {};
 ORIGINS[data] = 1;
+ORIGINS[adminUrl] = 1;
 
 cds.on('bootstrap', app => app.use ((req, res, next) => {
     if (req.headers.origin in ORIGINS) {
