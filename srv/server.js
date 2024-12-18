04b73bed4e73a7f8bceedeecdc92097aef7c9a34
Divya-Kaavian <divyadharshini.murugan@kaaviansys.com>
2024-12-18 03:58:57 +0000

    Updated the CORS implementation

--- srv/server.js
+++ srv/server.js
@@ -9,7 +9,7 @@ cds.on('bootstrap', app => app.use ((req, res, next) => {
     if (req.headers.origin in ORIGINS) {
     
     res.set('access-control-allow-origin', req.headers.origin)
-    if (req.method === 'OPTIONS') // preflight request
+    if (req.method === 'OPTIONS')
       return res.set('access-control-allow-methods', 'GET,HEAD,PUT,PATCH,POST,DELETE').end()
   }
   next()
