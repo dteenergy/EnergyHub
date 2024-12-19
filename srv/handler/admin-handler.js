a787077a01cbc8f462cf74bfadd04af537ebaf75
Divya-Kaavian <divyadharshini.murugan@kaaviansys.com>
2024-12-19 10:32:33 +0000

    Added the NoOfConsentReceived fields in the ApplicationDetail

--- srv/handler/admin-handler.js
+++ srv/handler/admin-handler.js
@@ -2,6 +2,7 @@ const cds = require('@sap/cds');
 const { generateConsentUrl } = require('./generate-consent-url');
 
 module.exports = cds.service.impl(async function DTEEnergyAdminPortal(srv) {
+    const {ApplicationConsent} = this.entities;
     // Method to Generate the Consent URL
     srv.on('GenerateUrl', 'ApplicationDetail', async ({ params: [id] }) => {
         // Get the AppId
@@ -12,6 +13,24 @@ module.exports = cds.service.impl(async function DTEEnergyAdminPortal(srv) {
 
         // Return the ConsentURL with the encryptedid.
         return generatedUrl;
+    }),
+
+    // Method to add the NoOfConsentReceived Field.
+    srv.after('READ', 'ApplicationDetail', async(data)=>{
+        // If data contains value
+        if(Array.isArray(data)){
+            for(const e of data){
+                // Check the ReferenceId with the AppId
+                const ConsentDetailCount = await cds.run(
+                    SELECT.from(ApplicationConsent)
+                    .where({AppRefId_AppId: e.AppId})
+                )
+                e.NoOfConsentReceived =  ConsentDetailCount.length;
+            }
+        } else {
+            // Set the NoOdConsentReceived as 0
+            data.NoOfConsentReceived = 0
+        }
     })
 
 })
