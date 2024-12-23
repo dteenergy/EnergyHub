f6a51d2c0300f0e2ed6b533596e704e990eb9d2d
Divya-Kaavian <divyadharshini.murugan@kaaviansys.com>
2024-12-23 06:42:37 +0000

    Changed the conditional statement

--- srv/handler/generate-consent-url.js
+++ srv/handler/generate-consent-url.js
@@ -15,7 +15,7 @@ const generateConsentUrl = async (appId, ApplicationDetail) => {
     const appDetail = await SELECT.from(ApplicationDetail).columns('AppId').where({ 'AppId': appId });
 
     // If the length is empty
-    if (appDetail.length === 0) throw { message: 'Application Details for this Id is not found. Invalid Application ID', code: 400 }
+    if (appDetail?.length === 0) throw { message: 'Application Details for this Id is not found. Invalid Application ID', code: 400 }
 
     // Encrypt the appid
     const encrAppId = await valueEncrypt(appId);
@@ -27,7 +27,7 @@ const generateConsentUrl = async (appId, ApplicationDetail) => {
 
   } catch (e) {
     if (e) {
-      throw { code: e.code, message: e.message };
+      throw { code: e?.code, message: e?.message };
     }
     throw { code: 500, message: 'Failed to generate the consent app url' }
   }
