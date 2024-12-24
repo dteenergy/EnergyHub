3bcf01374952a524c571f3c4c653ca10bb0086f6
Divya-Kaavian <divyadharshini.murugan@kaaviansys.com>
2024-12-24 07:28:43 +0000

    Completed the Logic for the Filter and Update

--- app/dte-energy-admin-portal/webapp/controller/ConsentsPage.controller.js
+++ app/dte-energy-admin-portal/webapp/controller/ConsentsPage.controller.js
@@ -2,45 +2,87 @@ sap.ui.define([
     "dteenergyadminportal/controller/BaseController",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-], (BaseController, Filter, FilterOperator) => {
+    "sap/m/MessageToast",
+     "sap/ui/comp/personalization/Controller",
+     "sap/m/MessageBox",
+], (BaseController, Filter, FilterOperator, MessageToast, PersonalizationController, MessageBox) => {
     "use strict";
 
     return BaseController.extend("dteenergyadminportal.controller.ConsentsPage", {
-        onInit() {},
+        onInit() {
+            // Set the MainModel for the view
+            this.getView().setModel("MainModel");
 
+            this.oPersonalizationController = new PersonalizationController({
+                table: this.byId("idApplicationConsentTable")
+              });
+        },
+         /**
+         * Opens the personalization dialog for the application table.
+        *
+        * @public
+        */
+        setupPersonalization: function () {
+            this.oPersonalizationController.openDialog();
+        },
+
+         /**
+         * Handles filter changes for the application table and
+         * applies the corresponding filters.
+         *
+         * @public
+         */
         onFilterChange : function(){
-            console.log("Handle the Filter change");
+            // Retrieve the application consent table and its binding
             const oTable = this.byId("idApplicationConsentTable")
             const oBinding = oTable.getBinding("items");
 
+            // Validate the binding
             if(!oBinding){
-                console.log("Binding for the table is not found");
+                MessageBox.error("Binding for the table is not found");
                 
                 return;
             }
 
+            /**
+             *  Get values from the filters inputs
+             *  Firstname, Lastname and Consentstatus
+             */
             const firstName = this.byId("idFilterFirstName").getValue();
             const lastName = this.byId("idFilterLastName").getValue();
-            const applicationStatus = this.byId("idFilterAppStatus").getValue();
+            const consentStatus = this.byId("idConsentStatusFilter").getSelectedKey();
 
+            // Array for filter
             const filterData = [];
 
-            if(firstName) filterData.push(new Filter({path: "AccountDetailRefId/FirstName", operator: FilterOperator.Contains, value1: firstName, caseSensitive: false}));
-            if(lastName) filterData.push(new Filter({path: "AccountDetailRefId/LastName", operator: FilterOperator.Contains, value1: lastName, caseSensitive: false}));
-            if(applicationStatus) filterData.push(new Filter("ApplicationStatus", FilterOperator.EQ, applicationStatus));
+            // Handle the filters value
+            if(firstName) filterData.push(new Filter({path: "FirstName", operator: FilterOperator.Contains, value1: firstName, caseSensitive: false}));
+            if(lastName) filterData.push(new Filter({path: "LastName", operator: FilterOperator.Contains, value1: lastName, caseSensitive: false}));
+            if(consentStatus) filterData.push(new Filter("ConsentStatus", FilterOperator.EQ, consentStatus));
 
+            // Combine filter logic.
             const combinedFilter = new Filter({
                 filters: filterData,
                 and: true
             })
 
+            // Bind the combined filter or clear filter.
             oBinding.filter(filterData.length > 0 ? combinedFilter : [])
             
         },
-        handleResponse : function(){
-            const oModel = this.getView().getModel("MainModel");
-            oModel.submitBatch("CustomGroupId").then(()=>sap.m.MessageToast.show("Application Status Updated Successfully")).catch(()=>sap.m.MessageToast.show("Application Status Failed to Update."))
 
+         /**
+         * Submits batch updates to the backend for saving changes to the application consent fields.
+         * And display the message(success/error).
+         *
+         * @public
+         */
+        handleResponse : function(){
+        const updateModel = this.getView().getModel("MainModel");
+
+        updateModel.submitBatch('CustomGroupId')
+        .then(() => MessageToast.show("Updated successfully!"))
+        .catch((err) => MessageToast.show("Updation failed : ", err))
         }
     });
 });
