e4a7c2b9ecf47380e9c113b26e23bd4063e33b89
bhoopeshkumar tharmarajan <bhoopeshkumar.tharmarajan@dteenergy.com>
2024-12-26 05:59:48 +0000

    DTE-26 :

--- app/dte-energy-admin-portal/webapp/controller/BuildingDetailPage.controller.js
+++ app/dte-energy-admin-portal/webapp/controller/BuildingDetailPage.controller.js
@@ -1,9 +1,47 @@
 sap.ui.define([
-  "dteenergyadminportal/controller/BaseController"
-], (BaseController) => {
+  "dteenergyadminportal/controller/BaseController",
+  "sap/ui/comp/personalization/Controller",
+], (BaseController, PersonalizationController) => {
   "use strict";
 
   return BaseController.extend("dteenergyadminportal.controller.BuildingDetailPage", {
-    onInit() {},
+    onInit() {
+      const { baseUrl, AppId } = this.getView().getViewData();
+      this.baseUrl = baseUrl;
+      console.log(AppId);
+      
+      // Create an OData V4 model using the constructed service URL
+      const model = new sap.ui.model.odata.v4.ODataModel({
+        serviceUrl: `${this.baseUrl}admin/service/`,
+        synchronizationMode: "None",
+        operationMode: "Server",
+      });
+
+      // Set the newly created model as the "MainModel" for this view
+      this.getView().setModel(model, "MainModel");
+
+      // Initialize the Personalization Controller for the application table
+      this.oPersonalizationController = new PersonalizationController({
+        table: this.byId("idBuildingTable")
+      });
+
+      const oTable = this.byId("idBuildingTable"); // Ensure this ID matches your XML table ID
+
+      // Apply a filter to fetch only BuildingDetail entries related to the selected AppId
+      const oBinding = oTable.getBinding("items");
+      const oFilter = new sap.ui.model.Filter("AppRefId_AppId", "EQ", AppId);
+
+      if (oBinding) {
+        oBinding.filter([oFilter]);
+      }
+    },
+    /**
+     * Opens the personalization dialog for the application table.
+     *
+     * @public
+     */
+    setupPersonalization: function () {
+      this.oPersonalizationController.openDialog();
+    },
   });
 });
