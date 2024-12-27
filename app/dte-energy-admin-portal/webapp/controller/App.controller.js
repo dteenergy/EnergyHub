6af3c51b82281fcc63077f6238b592c67f541aa5
Divya-Kaavian <divyadharshini.murugan@kaaviansys.com>
2024-12-27 04:41:06 +0000

    DTE Admin Header - Logout Button added in the Profile, handler to handle the logout

--- app/dte-energy-admin-portal/webapp/controller/App.controller.js
+++ app/dte-energy-admin-portal/webapp/controller/App.controller.js
@@ -5,6 +5,21 @@ sap.ui.define([
 
   return BaseController.extend("dteenergyadminportal.controller.App", {
       onInit() {
+      },
+      // Handle the AvatarPress
+      handleAvatarPress:function (oEvent){
+        const oButton = oEvent.getSource();
+        
+        // Find the menu by its ID and open it relative to the avatar button
+        const oMenu = this.byId('idAvatarMenu');
+        oMenu.openBy(oButton);
+      },
+
+      // Handle the logout
+      handleLogout : async function (){
+        console.log("Handle Logout");
+        
       }
+
   });
 });
