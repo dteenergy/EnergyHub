f5b65c004723fdf9543c3a505907af583e1331e5
Divya-Kaavian <divyadharshini.murugan@kaaviansys.com>
2024-12-30 03:53:25 +0000

    Cors implementation in the xs-app.json

--- srv/server.js
+++ srv/server.js
@@ -1,25 +1,27 @@
-const cds = require('@sap/cds');
-const cors = require('cors');
+// const cds = require('@sap/cds');
+// const cors = require('cors');
 
-// Fetch the whitelist from environment variables
-const whiteListedData = process.env.WHITELIST || ["*"];
+// // Fetch the whitelist from environment variables
+// const whiteListedData = process.env.WHITELIST || ["*"];
 
-const dynamicCorsHandler = function (req, callback) {
-  let corsOptions;
-  if (whiteListedData.indexOf(req.header('Origin')) !== -1) {    
-    // reflect (enable) the requested origin in the CORS response
-    corsOptions = { origin: true } 
-  } else {
-    // disable CORS for this request
-    corsOptions = { origin: false }
-  }
-  // callback expects two parameters: error and options
-  callback(null, corsOptions) 
-}
+// const dynamicCorsHandler = function (req, callback) {
+//   let corsOptions;
+//   if (whiteListedData.indexOf(req.header('Origin')) !== -1) {    
+//     // reflect (enable) the requested origin in the CORS response
+//     corsOptions = { origin: true } 
+//   } else {
+//     // disable CORS for this request
+//     corsOptions = { origin: false }
+//   }
+//   console.log(corsOptions);
+  
+//   // callback expects two parameters: error and options
+//   callback(null, corsOptions) 
+// }
 
-cds.on('bootstrap', async (app)=>{
-  // Access the CorsValidation
-    app.use(cors(dynamicCorsHandler));  
-})
+// cds.on('bootstrap', async (app)=>{
+//   // Access the CorsValidation
+//     app.use(cors(dynamicCorsHandler));  
+// })
 
-module.exports = cds.server;
+// module.exports = cds.server;
