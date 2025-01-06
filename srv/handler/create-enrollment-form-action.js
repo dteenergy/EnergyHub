e9f66c2a88cf781f64a5c42b00265202ea2dc0cb
kokila sivakumar <kokila.sivakumar@dteenergy.com>
2025-01-06 18:01:24 +0000

    changed the logic to generate the application number

--- srv/handler/create-enrollment-form-action.js
+++ srv/handler/create-enrollment-form-action.js
@@ -15,32 +15,58 @@ const createEnrollmentFormDetail = async (req, entity, tx) => {
     // Get the flag and prefix for the application number from environment variables
     const prefixFlag = process.env.APPNUM_PREFIX_ENABLED;
     const appNumPrefix = process.env.APPNUM_PREFIX;
+ 
+    // Define the base query
+    let query = SELECT.from(entity.ApplicationDetail)
 
-    // Fetch the last ApplicationNumber from ApplicationDetail entity
-    const applicationDetail = await SELECT.from(entity.ApplicationDetail)
       .columns('ApplicationNumber')
-      .orderBy('ApplicationNumber desc')
-      .limit(1);
 
+      .orderBy('ApplicationNumber desc')
+
+      .limit(1);
+    
+    // Add a condition for the prefix if prefixFlag is "Yes"
+
+    if (prefixFlag === "Y" && appNumPrefix) {
+
+      query = query.where({ ApplicationNumber: { like: `${appNumPrefix}%` } });
+
+    }
+    
+    // Fetch the last ApplicationNumber based on the query
+
+    const applicationDetail = await cds.run(query);
+    
+    console.log(applicationDetail);
+
+ 
     // Store the application number
     let applicationNumber;
-
-    // Check the recent application number exists
+    
+    // Check if the recent application number exists
     if (applicationDetail?.length > 0) {
       const lastAppNumber = applicationDetail[0]?.ApplicationNumber;
-
-      // Extract the numeric part and increment it
-      const numericPart = parseInt(lastAppNumber?.slice(1)) || 0;
-      const incrementedNumber = numericPart + 1;
-
-      // Generate the new application number with or without prefix.
-      const incrementedNumberStr = incrementedNumber.toString().padStart(9, '0');
+    
+      // Extract the numeric part by removing the prefix
+      const numericPart = lastAppNumber.replace(appNumPrefix, "");
+      const parsedNumber = parseInt(numericPart, 10) || 0;
+    
+      // Increment the numeric part
+      const incrementedNumber = parsedNumber + 1;
+    
+      // Pad the incremented number with leading zeros
+      const incrementedNumberStr = incrementedNumber.toString().padStart(numericPart.length, '0');
+    
+      // Generate the new application number with or without prefix
       applicationNumber = prefixFlag === 'Y' ? appNumPrefix.concat(incrementedNumberStr) : incrementedNumberStr;
     } else {
       // If no previous application number, start from 1 with or without the prefix
       const incrementedNumberStr = '000000001';
       applicationNumber = prefixFlag === 'Y' ? appNumPrefix.concat(incrementedNumberStr) : incrementedNumberStr;
     }
+
+    console.log(applicationNumber);
+    
 
     // Generate a unique AppId using uuid
     const AppId = uuidv4();
