30c316d2e30b270c2c23e9dda186364779882b25
kokila sivakumar <kokila.sivakumar@dteenergy.com>
2025-01-15 13:02:04 +0000

    keep the regex only for city, zipcode and account number remaining are commented

--- app/dteconsentappclient/webapp/variable/RegexAndMessage.js
+++ app/dteconsentappclient/webapp/variable/RegexAndMessage.js
@@ -7,12 +7,12 @@ sap.ui.define([], function(){
 			PhoneNumber: /^(?!0000000000$)\d{10}$/,
       Zipcode: /^(?!00000$)\d{5}$/,
       AccountNumber: /^(91|92)[0-9]{10}$/,
-      City: /^[a-zA-Z][a-zA-Z\s]{0,48}[a-zA-Z]$/,
-      Name: /^[a-zA-Z][a-zA-Z\s]{0,48}[a-zA-Z]$/,
-      Address: /^[a-zA-Z0-9][a-zA-Z0-9\s,./-]{0,48}[a-zA-Z0-9,/.-]$/,
-      AddrLineTwo: /^[a-zA-Z0-9][a-zA-Z0-9\s,./-]{0,48}[a-zA-Z0-9,/.-]$/,
-      Title: /^[a-zA-Z][a-zA-Z\s]{0,48}[a-zA-Z]$/,
-      SignedBy: /^[a-zA-Z][a-zA-Z\s]{0,48}[a-zA-Z]$/,
+      City: /^[a-zA-Z][a-zA-Z\s]{0,99}$/,
+      // Name: /^[a-zA-Z][a-zA-Z\s]{0,48}[a-zA-Z]$/,
+      // Address: /^[a-zA-Z0-9][a-zA-Z0-9\s,./-]{0,48}[a-zA-Z0-9,/.-]$/,
+      // AddrLineTwo: /^[a-zA-Z0-9][a-zA-Z0-9\s,./-]{0,48}[a-zA-Z0-9,/.-]$/,
+      // Title: /^[a-zA-Z][a-zA-Z\s]{0,48}[a-zA-Z]$/,
+      // SignedBy: /^[a-zA-Z][a-zA-Z\s]{0,48}[a-zA-Z]$/,
 		};
 
     const validationMessage = {
@@ -21,11 +21,11 @@ sap.ui.define([], function(){
       Zipcode: "Please provide a proper Zip Code",
       AccountNumber: "Please provide a proper Account Number",
       City: "Please provide a proper city name",
-      Name: "Please provide a proper Name",
-      Address: "Please provide a proper address",
-      AddrLineTwo: "Please provide a proper address",
-      Title: "Please provide proper Title",
-      SignedBy: "Please provide a proper Name"
+      // Name: "Please provide a proper Name",
+      // Address: "Please provide a proper address",
+      // AddrLineTwo: "Please provide a proper address",
+      // Title: "Please provide proper Title",
+      // SignedBy: "Please provide a proper Name"
     }
 
   return {regex, validationMessage}
