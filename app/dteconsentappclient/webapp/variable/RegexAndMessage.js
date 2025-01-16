06b8b3bef4c367b1dd40890fc161773471aa7c13
kokila sivakumar <kokila.sivakumar@dteenergy.com>
2025-01-16 17:41:45 +0000

    fixed the header size as it is in figma and removed the service.js file and unnecessary regex and its messages

--- app/dteconsentappclient/webapp/variable/RegexAndMessage.js
+++ app/dteconsentappclient/webapp/variable/RegexAndMessage.js
@@ -7,12 +7,7 @@ sap.ui.define([], function(){
 			PhoneNumber: /^(?!0000000000$)\d{10}$/,
       Zipcode: /^(?!00000$)\d{5}$/,
       AccountNumber: /^(91|92)[0-9]{10}$/,
-      City: /^[a-zA-Z][a-zA-Z\s]{0,99}$/,
-      // Name: /^[a-zA-Z][a-zA-Z\s]{0,48}[a-zA-Z]$/,
-      // Address: /^[a-zA-Z0-9][a-zA-Z0-9\s,./-]{0,48}[a-zA-Z0-9,/.-]$/,
-      // AddrLineTwo: /^[a-zA-Z0-9][a-zA-Z0-9\s,./-]{0,48}[a-zA-Z0-9,/.-]$/,
-      // Title: /^[a-zA-Z][a-zA-Z\s]{0,48}[a-zA-Z]$/,
-      // SignedBy: /^[a-zA-Z][a-zA-Z\s]{0,48}[a-zA-Z]$/,
+      City: /^[a-zA-Z][a-zA-Z\s]{0,99}$/
 		};
 
     const validationMessage = {
@@ -20,12 +15,7 @@ sap.ui.define([], function(){
       PhoneNumber: "Please provide a proper Phone Number",
       Zipcode: "Please provide a proper Zip Code",
       AccountNumber: "Please provide a proper Account Number",
-      City: "Please provide a proper city name",
-      // Name: "Please provide a proper Name",
-      // Address: "Please provide a proper address",
-      // AddrLineTwo: "Please provide a proper address",
-      // Title: "Please provide proper Title",
-      // SignedBy: "Please provide a proper Name"
+      City: "Please provide a proper city name"
     }
 
   return {regex, validationMessage}
