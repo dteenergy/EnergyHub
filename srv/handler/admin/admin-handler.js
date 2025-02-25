eebf0e0dabf4196f6b46ee676d4ec87fbb158414
bhoopeshkumar tharmarajan <bhoopeshkumar.tharmarajan@dteenergy.com>
2025-02-25 09:37:47 +0000

    DTE-83 : Move files into admin handler

--- srv/handler/admin/admin-handler.js
+++ srv/handler/admin/admin-handler.js
@@ -1,7 +1,7 @@
 const cds = require('@sap/cds');
 const { generateConsentUrl } = require('./generate-consent-url');
-const { LinkApplications } = require('../../utils/link-applications');
-const { readApplicationDetail } = require('../../utils/sort-application-detail');
+const { LinkApplications } = require('./link-applications');
+const { readApplicationDetail } = require('./sort-application-detail');
 
 module.exports = cds.service.impl(async function DTEEnergyAdminPortal(srv) {
   const { ApplicationConsent, ApplicationDetail } = this.entities;
