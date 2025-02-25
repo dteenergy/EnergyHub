610d89eada0d43c674b21f74f0688ff04f33a3e1
bhoopeshkumar tharmarajan <bhoopeshkumar.tharmarajan@dteenergy.com>
2025-02-25 11:49:00 +0000

    DTE-82 : AHandle the logic for already linked applications

--- srv/handler/admin/link-applications.js
+++ srv/handler/admin/link-applications.js
@@ -15,6 +15,25 @@ const LinkApplications = async (req) => {
     // Access the CDS entities
     const entity = entities;
 
+    // Retrieve existing applications to check for already linked applications
+    const existingApplications = await cds.run(
+      SELECT.from(entity.ApplicationDetail)
+        .columns(['ApplicationNumber', 'LinkId'])
+        .where({ ApplicationNumber: { in: selectedApplicationNumbers } })
+    );
+
+    // Filter out applications that already have a LinkId assigned
+    const alreadyLinkedApplications = existingApplications.filter(app => app.LinkId);
+
+    // If any applications are already linked, return a warning message
+    if (alreadyLinkedApplications.length > 0) {
+      const linkedAppNumbers = alreadyLinkedApplications.map(app => app.ApplicationNumber).join(", ");
+      return {
+        message: `The following applications are already linked: ${linkedAppNumbers}. Please unlinked applications.`,
+        statusCode: 400
+      };
+    };
+
     // Execute the update operation on the ApplicationDetail entity
     const result = await cds.run(
       UPDATE(entity.ApplicationDetail)
@@ -24,7 +43,8 @@ const LinkApplications = async (req) => {
 
     // Check if any records were updated
     if(result === 0) return {message: 'No records found for the provided AppIds', statusCode: 404};
-    else return {message: 'Successfully updated!', statusCode:200};
+
+    return {message: 'Successfully updated!', statusCode: 200};
   } catch (error) {
     // Handle any errors that occurred during the update operation
     return { message: 'An error occurred while updating LinkId', statusCode: 500 };
